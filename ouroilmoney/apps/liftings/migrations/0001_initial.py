5536d207f3a07ef5cd619933b04df89d288fd01a
obengwilliam <obeng.kojo.william@gmail.com>
2014-10-06 13:20:37 +0000

    add migrations for liftings

---
+++ ouroilmoney/apps/liftings/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reports', '0005_auto_20141006_0644'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Lifting',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('is_published', models.BooleanField(default=False, verbose_name=b'publish')),
+                ('summary', models.TextField(help_text=b'Please copy and past summary of Report here.\n Optional', null=True, blank=True)),
+                ('date', models.DateField()),
+                ('volume_of_lifting', models.IntegerField()),
+                ('selling_price', models.DecimalField(max_digits=10, decimal_places=3)),
+                ('lifting_proceed', models.DecimalField(max_digits=19, decimal_places=3)),
+                ('report', models.ForeignKey(to='reports.ConfirmReport')),
+            ],
+            options={
+                'ordering': ('-date',),
+                'verbose_name': 'Lifting from Report',
+                'verbose_name_plural': 'Liftings from Reports',
+            },
+            bases=(models.Model,),
+        ),
+    ]
