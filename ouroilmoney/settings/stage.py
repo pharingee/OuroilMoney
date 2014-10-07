4440dbf0860e3acf11580653c8f2b245418cf1af
obengwilliam <obeng.kojo.william@gmail.com>
2014-10-07 08:29:30 +0000

    add swagger setti

--- ouroilmoney/settings/stage.py
+++ ouroilmoney/settings/stage.py
@@ -17,3 +17,20 @@ ALLOWED_HOSTS=os.environ.get('ALLOWED_HOSTS', '.herokuapp.com').split(':')
 
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+
+
+SWAGGER_SETTINGS = {
+    "exclude_namespaces": [],
+    "api_version": '1.0',  # Specify your API's version
+    "enabled_methods": [  # Specify which methods to enable in Swagger UI
+        'get',
+        'post',
+        'put',
+        'patch',
+        'delete'
+    ],
+    "api_key": '', # An API key
+    "is_authenticated": True,  # Set to True to enforce user authentication,
+    "is_superuser": True,  # Set to True to enforce admin only access
+}
