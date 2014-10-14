18ff8f89cb834c374d7df3bc056817235d5c6f67
obengwilliam <obeng.kojo.william@gmail.com>
2014-10-14 21:15:19 +0000

    add revenues/title

--- ouroilmoney/apps/revenues/api/serializers.py
+++ ouroilmoney/apps/revenues/api/serializers.py
@@ -1,16 +1,8 @@
 from rest_framework import serializers
 from ouroilmoney.apps.revenues.models import AnnualBudgetReportRevenue
 
-
 class AnnualBudgetRevenueSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AnnualBudgetReportRevenue
         fields = ('id', 'title', 'amount', 'year')
-
-
-class RevenueTitleSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = AnnualBudgetReportRevenue
-        fields = ('title',)
--- ouroilmoney/apps/revenues/api/views.py
+++ ouroilmoney/apps/revenues/api/views.py
@@ -1,18 +1,20 @@
 from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 from ouroilmoney.apps.revenues.models import AnnualBudgetReportRevenue
 from ouroilmoney.apps.revenues.api.serializers import (
-    AnnualBudgetRevenueSerializer,
-    RevenueTitleSerializer)
+    AnnualBudgetRevenueSerializer,)
+
 
 
 class AnnualBudgetRevenueViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = AnnualBudgetReportRevenue.objects.exclude(
         is_published=False).order_by('title')
-    lookup_field = 'title'
     serializer_class = AnnualBudgetRevenueSerializer
 
+    @list_route(methods=["get"])
+    def titles(self, request):
+        title=AnnualBudgetReportRevenue.objects.values('title').distinct()
+        return  Response(title)
 
-class RevenueTitleViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = AnnualBudgetReportRevenue.objects.values('title').distinct()
-    serializer_class =  RevenueTitleSerializer
