d55224d9170b3aaf882a554de81041eda8ff3846
obengwilliam <obeng.kojo.william@gmail.com>
2014-10-14 06:15:15 +0000

    add orderby title to annual sectors and other sectors in veiws

--- ouroilmoney/apps/projects/api/views.py
+++ ouroilmoney/apps/projects/api/views.py
@@ -12,20 +12,24 @@ from ouroilmoney.apps.projects.api.serializers import (
 
 
 class AnnualBudgetSectorViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = AnnualBudgetSector.objects.exclude(is_published=False)
+    queryset = AnnualBudgetSector.objects.exclude(
+        is_published=False).order_by('title')
     serializer_class = AnnualBudgetSectorSerializer
 
 
 class ConfirmSectorViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = ConfirmSector.objects.exclude(is_published=False)
+    queryset = ConfirmSector.objects.exclude(
+        is_published=False).order_by(' annual_budget_sector__title')
     serializer_class = ConfirmSectorSerializer
 
 
 class AnnualBudgetProjectViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = AnnualBudgetProject.objects.exclude(is_published=False)
+    queryset = AnnualBudgetProject.objects.exclude(
+        is_published=False)
     serializer_class = AnnualBudgetProjectSerializer
 
 
 class ConfirmProjectViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = ConfirmProject.objects.exclude(is_published=False)
+    queryset = ConfirmProject.objects.exclude(
+        is_published=False)
     serializer_class = ConfirmProjectSerializer
