0ba3afce23f6c8117cfd08d0a1e465bb5416d278
obengwilliam <obeng.kojo.william@gmail.com>
2014-12-15 10:02:51 +0000

    add currency to allocation admin

--- ouroilmoney/apps/allocations/admin.py
+++ ouroilmoney/apps/allocations/admin.py
@@ -7,15 +7,29 @@ from ouroilmoney.apps.allocations.models import (
 
 @admin.register(ConfirmAllocation)
 class ConfirmAllocationAdmin(admin.ModelAdmin):
-    fields = ['annual_budget_allocation', 'report', 'amount', 'is_published']
+    fields = [
+        'annual_budget_allocation',
+        'report',
+        'currency',
+        'amount',
+        'is_published']
+
     list_display = (
         'report',
+        'currency',
         'allocated_amount',
         'annual_budget_allocation',
-        'annual_budget_allocation_amount', 'is_published')
+        'annual_budget_allocation_amount',
+        'is_published')
 
 
 @admin.register(AnnualBudgetAllocation)
 class AllocationAdmin(admin.ModelAdmin):
-    fields = ['title', 'amount', 'report', 'is_published']
-    list_display = ('title', 'report', 'allocated_amount', 'is_published')
+    fields = ['title', 'currency', 'amount', 'report', 'is_published']
+
+    list_display = (
+        'title',
+        'report',
+        'currency',
+        'allocated_amount',
+        'is_published')
