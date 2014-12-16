1a1d2710435cb41c417f34a4c0ac06dea1459925
obengwilliam <obeng.kojo.william@gmail.com>
2014-12-16 10:56:28 +0000

    add img_rounded to image

--- ouroilmoney/apps/projects/models.py
+++ ouroilmoney/apps/projects/models.py
@@ -158,7 +158,7 @@ class ConfirmProject(ProjectModel):
 
     def admin_image(self):
             if self.image:
-                return '<img src="%s"/>' %self.image.url
+                return '<img class="img-rounded" src="%s"/>' %self.image.url
     admin_image.allow_tags = True
 
     @property
