7e87f36ad9fbf3115e9aa32423304c770b8c1ac7
obengwilliam <obeng.kojo.william@gmail.com>
2015-02-18 12:27:03 +0000

    change sms view sets

--- ouroilmoney/utils/api/views.py
+++ ouroilmoney/utils/api/views.py
@@ -11,25 +11,30 @@ from rest_framework.status import (
 
 from  ouroilmoney.utils.api.serializers import SmsMessageSerializer
 from rest_framework import viewsets
-
-@api_view(['GET'])
-@permission_classes((AllowAny,))
-def sms(request):
-    try:
-        user = request.REQUEST.get('from')
-        message = request.REQUEST.get('text')
-
-        sms = SmsMessage.objects.create(user=user, message=message)
-        sms.save()
-
-        return Response(
-            'Your message is noted. Thank you.', status=HTTP_200_OK)
-    except Exception:
-        return Response('Could not send message', status=HTTP_400_BAD_REQUEST)
+from rest_framework.decorators import list_route
 
 
 class SmsMessageViewSet(viewsets.ReadOnlyModelViewSet):
 
     serializer_class = SmsMessageSerializer
     queryset = SmsMessage.objects.filter(is_published=True)
+
+
+    @list_route(methods=["get"])
+    def sms(self, request):
+        """
+        Receive sms from smsgh
+        """
+        try:
+           user = request.REQUEST.get('from')
+           message = request.REQUEST.get('text')
+
+           sms = SmsMessage.objects.create(user=user, message=message)
+           sms.save()
+
+           return Response(
+               'Your message is noted. Thank you.', status=HTTP_200_OK)
+        except Exception:
+           return Response('Could not send message', status=HTTP_400_BAD_REQUEST)
+
 
