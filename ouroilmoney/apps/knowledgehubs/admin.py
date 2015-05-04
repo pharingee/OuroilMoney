2a6c9000bca33c8a2f2a2ce46d59a04a327821d6
obengwilliam <obeng.kojo.william@gmail.com>
2015-05-04 11:55:12 +0000

    add(subcategories): to admin

--- ouroilmoney/apps/knowledgehubs/admin.py
+++ ouroilmoney/apps/knowledgehubs/admin.py
@@ -5,12 +5,13 @@ from ouroilmoney.apps.knowledgehubs.models import KnowledgeHub
 # Register your models here.
 @admin.register(KnowledgeHub)
 class KnowledgeHubAdmin(admin.ModelAdmin):
-    fields = ['title', 'date', 'document', 'category', 'is_published']
+    fields = ['title', 'date', 'document', 'category', 'subcategory', 'is_published']
     list_display = (
         'title',
         'category',
         'date',
+        'subcategory',
         'report_has_document',
         'is_published')
 
-    list_filter = ('date', 'is_published', 'created')
+    list_filter = ('date', 'is_published', 'subcategory', 'created')
