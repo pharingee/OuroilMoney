fd6f7b3d9dd71e73cfd7ab04c565dacb42c737f0
obengwilliam <obeng.kojo.william@gmail.com>
2015-05-05 22:09:13 +0000

    change subcategory to subcategories

---
+++ ouroilmoney/apps/liftings/migrations/0019_auto_20150505_2147.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('liftings', '0018_auto_20150505_2131'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='lifting',
+            name='field',
+            field=models.ForeignKey(related_name=b'liftings', to='utils.Field'),
+        ),
+        migrations.AlterField(
+            model_name='lifting',
+            name='partner',
+            field=models.ForeignKey(related_name=b'liftings', to='utils.Partner'),
+        ),
+    ]
