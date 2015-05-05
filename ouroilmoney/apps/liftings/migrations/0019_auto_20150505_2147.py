ff0a918c8ea5d026edeb12ad7797f9e59b329c26
obengwilliam <obeng.kojo.william@gmail.com>
2015-05-05 22:18:50 +0000

    change subcategory to subcategories

--- ouroilmoney/apps/liftings/migrations/0019_auto_20150505_2147.py
+++ ouroilmoney/apps/liftings/migrations/0019_auto_20150505_2147.py
@@ -14,11 +14,11 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='lifting',
             name='field',
-            field=models.ForeignKey(related_name=b'liftings',null=True, blank=True to='utils.Field'),
+            field=models.ForeignKey(related_name=b'liftings', null=True, blank=True, to='utils.Field'),
         ),
         migrations.AlterField(
             model_name='lifting',
             name='partner',
-            field=models.ForeignKey(related_name=b'liftings', null=True, blank=Trueto='utils.Partner'),
+            field=models.ForeignKey(related_name=b'liftings', null=True, blank=True, to='utils.Partner'),
         ),
     ]
