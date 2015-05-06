7d3f8b075d0bf23d0cc54b601cd621bcb1521586
obengwilliam <obeng.kojo.william@gmail.com>
2015-05-06 00:26:54 +0000

    removed unecessary endpoints

--- ouroilmoney/apps/liftings/api/views.py
+++ ouroilmoney/apps/liftings/api/views.py
@@ -13,30 +13,3 @@ class LiftingViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Lifting.objects.exclude(is_published=False)
     serializer_class = LiftingSerializer
-
-    @list_route(methods=["get"])
-    def jubilee_partners(self, request):
-        """
-        Get liftings related to jubilee
-        """
-        jubilee_partners = Lifting.objects.filter(partner='GJ')
-        serializer = LiftingSerializer(jubilee_partners, many=True)
-        return Response(serializer.data)
-
-    @list_route(methods=["get"])
-    def ten_partners(self, request):
-        """
-        Get liftings related to jubilee
-        """
-        ten_partners = Lifting.objects.filter(partner='TN')
-        serializer = LiftingSerializer(ten_partners, many=True)
-        return Response(serializer.data)
-
-    @list_route(methods=["get"])
-    def other_partners(self, request):
-            """
-            Get lifting related to other partners
-            """
-            other_partners = Lifting.objects.exclude(partner='GJ')
-            serializer = LiftingSerializer(other_partners,many=True)
-            return Response(serializer.data)
