22cd4b6978366183c1ca40239bfb81c2fca37ba4
obengwilliam <obeng.kojo.william@gmail.com>
2015-05-20 01:56:19 +0000

    ready aws

--- ouroilmoney/settings/prod.py
+++ ouroilmoney/settings/prod.py
@@ -32,3 +32,29 @@ SWAGGER_SETTINGS = {
     "is_authenticated": True,  # Set to True to enforce user authentication,
     "is_superuser": True,  # Set to True to enforce admin only access
 }
+
+
+
+
+STATICFILES_STORAGE = 'ouroilmoney.utils.s3.StaticRootS3BotoStorage'
+
+DEFAULT_FILE_STORAGE ='ouroilmoney.utils.s3.MediaRootS3BotoStorage'
+
+THUMBNAIL_DEFAULT_STORAGE = DEFAULT_FILE_STORAGE
+
+AWS_ACCESS_KEY_ID= os.environ.get('AWS_ACCESS_KEY_ID')
+
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_QUERYSTRING_AUTH = False
+
+STATIC_DIRECTORY = '/static/'
+
+MEDIA_DIRECTORY = '/media/'
+
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+
+STATIC_URL = S3_URL + STATIC_DIRECTORY
+MEDIA_URL = S3_URL + MEDIA_DIRECTORY
